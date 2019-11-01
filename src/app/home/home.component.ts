9455af3bb514563dfe01a8b971f8627f1774d766
nilschap <nils.chapman@gmail.com>
2019-11-01 08:23:47 -0700

    Login Function Added to http request.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild("startTime", { static: true }) 
+  startTime: ElementRef;
+
+  @ViewChild("endTime", { static: true }) 
+  endTime: ElementRef;
+
+  displayLogin: boolean = true;
+  displayHome: boolean = false;
+  displayRegister: boolean = false;
+
+  print() {
+    console.log(this.startTime.nativeElement.value+this.endTime.nativeElement.value)
+  }
 
   constructor() { }
 
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,  HttpHeaders } from '@angular/common/http';
+import { User } from './Models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
-}
+  url: string = "localhost:8081/";
+
+
+   httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'my-auth-token'
+    })
+  };
+      Login(user: User, callback){
+        this.http.post<User>(this.url, user, this.httpOptions);
+      callback("test");
+      }
+
+
+  
+
+
+ 
+
+  constructor(private http: HttpClient) { 
+    this.Login({username: "Nils", password: "123"}, function (value) {
+      console.log(value);
+    })
+    }
+  }
